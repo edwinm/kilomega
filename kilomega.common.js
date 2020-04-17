00bd85a18506a84d8a1d4115a87efd7222319c8f
Edwin Martin <edwin@bitstorm.org>
2020-04-17 16:25:38 +0200

    Better describe which units are output

--- kilomega.common.js
+++ kilomega.common.js
@@ -9,7 +9,7 @@
 const units = [{ prefix: "", symbol: "", iecPrefix: "", iecSymbol: "" }, { prefix: "kilo", symbol: "k", iecPrefix: "kibi", iecSymbol: "Ki" }, { prefix: "mega", symbol: "M", iecPrefix: "mebi", iecSymbol: "Mi" }, { prefix: "giga", symbol: "G", iecPrefix: "gibi", iecSymbol: "Gi" }, { prefix: "tera", symbol: "T", iecPrefix: "tebi", iecSymbol: "Ti" }, { prefix: "peta", symbol: "P", iecPrefix: "pebi", iecSymbol: "Pi" }, { prefix: "exa", symbol: "E", iecPrefix: "exbi", iecSymbol: "Ei" }, { prefix: "zetta", symbol: "Z", iecPrefix: "zebi", iecSymbol: "Zi" }, { prefix: "yotta", symbol: "Y", iecPrefix: "yobi", iecSymbol: "Yi" }];
 
 /**
- * Outputs an object with amount and unit.
+ * Outputs an object with amount and si and iec units.
  * @param {number} amount Number of bits or bytes
  */
 function kilomega(amount) {
